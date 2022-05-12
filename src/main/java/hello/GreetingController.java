66240b397fad0586e73dc76facc92040260e7692
Flowey-null <58041029+Flowey-null@users.noreply.github.com>
2022-05-12 15:20:23 +0200

    Premier commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -16,4 +16,10 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+     @RequestMapping("/movies")
+     public String movies() {
+       return "movies";
+   }
+    
 }
